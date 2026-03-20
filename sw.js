408ba3b33e2c790db8f8e4f7cc1e49b7bb3a4da7
joaopvmx <47433000+joaopvmx@users.noreply.github.com>
2026-03-20 14:27:10 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,29 +1,5 @@
-const CACHE = 'carteirinha-v1';
-const ASSETS = [
-  './carteirinha.html',
-  './manifest.json'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS))
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => {
-      return cached || fetch(e.request).catch(() => caches.match('./carteirinha.html'));
-    })
-  );
-});
+const CACHE='carteirinha-v2';
+const ASSETS=['./carteirinha.html','./manifest.json'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).catch(()=>caches.match('./carteirinha.html'))));});
